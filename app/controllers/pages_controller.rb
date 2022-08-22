57309ff440ec4214b7f5011561949aed854826f4
Roel de Jong <Roel4811@hotmail.com>
2022-08-22 10:21:15 +0200

    add devise project

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
+  end
+
+  def about
+    if user_signed_in
+      @flats = current_user.flats
+    end
   end
 end
